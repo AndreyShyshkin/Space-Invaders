2b73588055d874c994dfeb5dee0021d199abb70c
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-06-11 20:21:09 +0300

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -5,27 +5,27 @@ let soundVolume = 0.5;
 $(".soundMusic").on("click", function(){
     if ($("#bg-audio")[0].paused){
         $("#bg-audio")[0].play();
-        $("#soundMusicImg").attr("src", "/images/sound on.png");
+        $("#soundMusicImg").attr("src", "./images/sound on.png");
     } else {
         $("#bg-audio")[0].pause();
-        $("#soundMusicImg").attr("src", "/images/sound off.png");
+        $("#soundMusicImg").attr("src", "./images/sound off.png");
     }
 });
 
 $(".soundEffect").on("click", function(){     
     if (soundVolume > 0){
         soundVolume = 0;
-        $("#soundEffectImg").attr("src", "/images/sound off.png");
+        $("#soundEffectImg").attr("src", "./images/sound off.png");
     }else {
         soundVolume = 1;
-        $("#soundEffectImg").attr("src", "/images/sound on.png");
+        $("#soundEffectImg").attr("src", "./images/sound on.png");
     }
 })
 
 
 
 function playBoom() {
-    let audio_boom = new Audio('/audio/boom.wav');
+    let audio_boom = new Audio('../audio/boom.wav');
 
     audio_boom.volume = soundVolume;
 
@@ -33,7 +33,7 @@ function playBoom() {
 }
 
 function playShot() {
-    let audio_shot = new Audio('/audio/shot.wav');
+    let audio_shot = new Audio('../audio/shot.wav');
 
     audio_shot.volume = soundVolume;
 
